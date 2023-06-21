e5ae43c315b6519656e89342e2384614db25a3e6
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-06-21 18:13:20 +0300

    initiall commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <h1>Hello word</h1>;
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+import 'react-toastify/dist/ReactToastify.css';
+import App from 'components/App/App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Currency-Converter">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
